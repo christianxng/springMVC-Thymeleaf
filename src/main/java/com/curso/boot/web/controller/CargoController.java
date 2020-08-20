a193f44b3147218d9208dbc6242eab34c57d3cba
Christian Rodrigues <christianmoura.moura@hotmail.com>
2020-08-20 17:14:58 -0300

    Controller e View de todas entidades + Validação de campos formulário.

--- src/main/java/com/curso/boot/web/controller/CargoController.java
+++ src/main/java/com/curso/boot/web/controller/CargoController.java
@@ -2,9 +2,12 @@ package com.curso.boot.web.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,8 @@ import com.curso.boot.domain.Cargo;
 import com.curso.boot.domain.Departamento;
 import com.curso.boot.service.CargoService;
 import com.curso.boot.service.DepartamentoService;
+
+import groovy.lang.Binding;
 
 @Controller
 @RequestMapping("/cargos")
@@ -39,7 +44,10 @@ public class CargoController {
 	}
 	
 	@PostMapping("/salvar")
-	public String salvar(Cargo cargo, RedirectAttributes attr) {
+	public String salvar(@Valid Cargo cargo, BindingResult result,RedirectAttributes attr) {
+		if(result.hasErrors()) {
+			return "/cargo/cadastro";
+		}		
 		serviceCargo.salvar(cargo);
 		attr.addFlashAttribute("success", "Cargo inserido com sucesso.");
 		return "redirect:/cargos/cadastrar";
@@ -47,12 +55,17 @@ public class CargoController {
 	
 	@GetMapping("/editar/{id}")
 	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		
 		model.addAttribute("cargo", serviceCargo.buscarPorId(id));
 		return "/cargo/cadastro";
 	}
 	
 	@PostMapping("/editar")
-	public String editar(Cargo cargo, RedirectAttributes attr) {
+	public String editar(@Valid Cargo cargo, BindingResult result, RedirectAttributes attr) {
+		
+		if(result.hasErrors()) {
+			return "/cargo/cadastro";
+		}	
 		serviceCargo.editar(cargo);
 		attr.addFlashAttribute("success", "Cargo editado com sucesso.");
 		return "redirect:/cargos/cadastrar";
